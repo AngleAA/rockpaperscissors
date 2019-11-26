2f1238788572dbf454ab79a5d022cdb04e8a3c50
AngleAA <jaedritzangel@gmail.com>
2019-11-26 23:06:36 +0000

    more stuff

--- js/script.js
+++ js/script.js
@@ -4,8 +4,31 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+$("#rock").click(function(){
+    
+    let com = Math.random();
 
+    if ( com >= 2/3 && com <= 1) {
+        $("#computerChoice").text("Scissors");
+    } else if (com >= 1/3 && com <= 2/3) {
+        $("#computerChoice").text("Rock");
+    } else if (com >= 1/3 && com <= 0) {
+        $("#computerChoice").text("Paper");
+    } else {
+        $("body").alert("rps broke");
+    }
 
+    $("#userChoice").html("Rock");
+
+});
+
+$("#paper").click(function(){
+   
+});
+
+$("#scissors").click(function(){
+
+});
 
 // DOCUMENT READY FUNCTION BELOW
 
